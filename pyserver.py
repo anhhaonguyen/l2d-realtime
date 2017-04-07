3cd22fab7832fc181d4f902e72343b0c458e16e4
anhhaonguyen <haonguyen1008@gmail.com>
2017-04-07 15:30:29 +0700

    fix db gone error

--- pyserver.py
+++ pyserver.py
@@ -1,35 +1,18 @@
-import math, json, socket
-import os, errno
+import json
 import peewee
 from peewee import *
 
 from flask import Flask, request
+
 app = Flask(__name__)
 
 
-db = MySQLDatabase('l2d_points', user='l2d', passwd='cubin123')
-
-class Points(peewee.Model):
-    lat = peewee.FloatField()
-    lng = peewee.FloatField()
-    country_code = peewee.CharField()
-
-    class Meta:
-        database = db
-
-# point = Points(lat='10.6', lng='107.1', country_code='AT')
-# point.save()
-
-# for p in Points.filter(country_code='AT'):
-#     print(p.lat)
-
-
-# =================================================================================
 @app.route('/')
 def index():
     return ''
+# ======================================
 
-# =================================================================================
+
 @app.route('/create', methods=['POST'])
 def create():
     data = request.json
@@ -40,29 +23,43 @@ def create():
     lat = coordinates[1]
 
     if country_code is None or lat is None or lng is None:
-        dict = {
+        info = {
             "error_code": 2,
             "message": "Invalid JSON request"
         }
-        return json.dumps(dict)
+        return json.dumps(info)
+
+    db = MySQLDatabase('l2d_points', user='l2d', passwd='cubin123')
+
+    class Points(peewee.Model):
+        lat = peewee.FloatField()
+        lng = peewee.FloatField()
+        country_code = peewee.CharField()
+
+        class Meta:
+            database = db
 
     existing_points = Points.filter(country_code=country_code)
 
     if len(existing_points) > 0:
-        dict = {
-                "error_code": 1,
-                "message": "Existed country code"
+        info = {
+            "error_code": 1,
+            "message": "Existed country code"
         }
-        return json.dumps(dict)
+        db.close()
+        return json.dumps(info)
     else:
         point = Points(lat=lat, lng=lng, country_code=country_code)
         point.save()
-        dict = {
+        info = {
             "error_code": 0,
             "message": "Create successfully"
         }
-        return json.dumps(dict)
-# =================================================================================
+        db.close()
+        return json.dumps(info)
+# ======================================
+
+
 @app.route('/update', methods=['POST'])
 def update():
     data = request.json
@@ -72,11 +69,21 @@ def update():
     lat = coordinates[1]
 
     if country_code is None or lat is None or lng is None:
-        dict = {
+        info = {
             "error_code": 2,
             "message": "Invalid JSON request"
         }
-        return json.dumps(dict)
+        return json.dumps(info)
+
+    db = MySQLDatabase('l2d_points', user='l2d', passwd='cubin123')
+
+    class Points(peewee.Model):
+        lat = peewee.FloatField()
+        lng = peewee.FloatField()
+        country_code = peewee.CharField()
+
+        class Meta:
+            database = db
 
     for p in Points.filter(country_code=country_code):
         if p.country_code == country_code:
@@ -84,45 +91,74 @@ def update():
             p.lng = lng
             p.country_code = country_code
             p.save()
-            dict = {
+            info = {
                 "error_code": 0,
                 "message": "Update successfully"
             }
-            return json.dumps(dict)
-    dict = {
+            db.close()
+            return json.dumps(info)
+    info = {
         "error_code": 3,
         "message": "Not found requesting point"
     }
-    return json.dumps(dict)
+    db.close()
+    return json.dumps(info)
+# ======================================
 
-# =================================================================================
+
 @app.route('/delete', methods=['POST'])
 def delete():
     data = request.json
     country_code = data['country_code']
 
+    db = MySQLDatabase('l2d_points', user='l2d', passwd='cubin123')
+
+    class Points(peewee.Model):
+        lat = peewee.FloatField()
+        lng = peewee.FloatField()
+        country_code = peewee.CharField()
+
+        class Meta:
+            database = db
+
     for p in Points.filter(country_code=country_code):
         if p.country_code == country_code:
             p.delete_instance()
-            dict = {
+            info = {
                 "error_code": 0,
                 "message": "Delete successfully"
             }
-            return json.dumps(dict)
-# =================================================================================
+            db.close()
+            return json.dumps(info)
+# ======================================
+
+
 @app.route('/points', methods=['GET'])
 def points():
-    dict = {}
-    pointArray = []
+    info = {}
+    point_array = []
+    db = MySQLDatabase('l2d_points', user='l2d', passwd='cubin123')
+
+    class Points(peewee.Model):
+        lat = peewee.FloatField()
+        lng = peewee.FloatField()
+        country_code = peewee.CharField()
+
+        class Meta:
+            database = db
+
     for p in Points.select():
         point = {
             "type": "Point",
             "coordinates": [p.lng, p.lat],
             "country_code": p.country_code
         }
-        pointArray.append(point)
+        point_array.append(point)
 
-    dict['result'] = pointArray
-    return json.dumps(dict)
+    info['result'] = point_array
+    db.close()
+    return json.dumps(info)
+# ======================================
 
-app.run(host='localhost', port=5000)
+
+app.run(host='0.0.0.0', port=5000)
